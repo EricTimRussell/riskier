9916a9e005da7819bc85e92736cf7fddba578410
Eric Russell <Erictim.Russell@live.com>
2022-11-28 14:46:39 -0700

    ...

--- riskier.client/src/env.js
+++ riskier.client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'erictim.us.auth0.com'
+export const clientId = '6xePrWzHTnB6GgciYbt92k0N2gX831rr'
+export const audience = 'https://myapp.com'
+
